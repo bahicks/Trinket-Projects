5e361ac5eeb7552d5a1e00e222b9e902a24296ec
bahicks <bljhicks@yahoo.com>
2019-01-16 23:26:49 -0600

    Removed unused code

--- RotateGear.py
+++ RotateGear.py
@@ -18,18 +18,6 @@ WHITE = WHITE = (255, 255, 255)
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False)
 
-""" def wheel(pos):
-	# Input a value 0 to 255 to get a color value.
-	# The colours are a transition r - g - b - back to r.
-	if pos < 0 or pos > 255:
-		return (0, 0, 0)
-	if pos < 85:
-		return (255 - pos * 3, pos * 3, 0)
-	if pos < 170:
-		pos -= 85
-		return (0, 255 - pos * 3, pos * 3)
-	pos -= 170
-	return (pos * 3, 0, 255 - pos * 3) """
 
 # def color_chase(color, wait):
 # 	for i in range(num_pixels):
